daabe570dd3442edd3e77a43db265b9e562cedad
BuildTools <unconfigured@null.spigotmc.org>
2020-11-04 18:04:51 -0500

    done

--- DCP 267.py
+++ DCP 267.py
@@ -15,10 +15,13 @@
 # .....Q..
 # You should return True, since the bishop is attacking the king diagonally.
 
+#=====================================================================================================================
 
 # Solution:
-#   We assume there can only be one king on the board because there is no way to differentiate between white and
+#   We assume there can only be one king on the 'board' because there is no way to differentiate between white and
 #       black king
+#   We assume the bottom of the 'board' is where we started for the sake of pawn movement
+#   We assume pawns will not make it/ or begin in the top row to be converted into another piece
 
 class Chess:
     def __init__(self, board):
@@ -53,7 +56,9 @@ class Chess:
         """
         for piece in self.pieces:
             if piece[0] == 'Q':
-                self.queen([piece[1], piece[2]])
+                # Queen uses rook + bishops checks
+                self.bishops([piece[1], piece[2]])
+                self.rook([piece[1], piece[2]])
             if piece[0] == 'R':
                 self.rook([piece[1], piece[2]])
             if piece[0] == 'B':
@@ -63,20 +68,21 @@ class Chess:
             if piece[0] == 'P':
                 self.pawn([piece[1], piece[2]])
 
-        if self.check == True:
+        if self.check:
             print("You are in check")
         else:
             print("You are not in check")
 
-    def queen(self, pos):
+    def rook(self, pos):
         """
-        queen: check all cardinal directions as well as diagonals looking for king
-        :return: True if king is in check/ Else False
+        rook: Checks all cardinal directions around the rook for king
+        :param pos: list of two ints containing the position of the rook
+        :return: N/A
         """
         # If they are in the same row, checks left or right
         if pos[0] == self.king_y:
             if pos[1] > self.king_x:
-                for i in range(pos[1]-1, self.king_x, -1):
+                for i in range(pos[1] - 1, self.king_x, -1):
                     if self.board[pos[0]][i] != ".":
                         return
                 self.check = True
@@ -103,27 +109,152 @@ class Chess:
                 self.check = True
                 return
 
-        # If they are in the same diagonal, checks diagonal
+    def bishops(self, pos):
+        """
+        bishops: checks all diagonals around piece for king
+        :param pos: list of two ints containing the position of the bishops
+        :return: N/A
+        """
+        # If on same height cannot attack
+        if pos[0] == self.king_y:
+            return
+        # If on same row cannot attack
+        if pos[1] == self.king_x:
+            return
 
+        # If bishop above king
+        if pos[0] < self.king_y:
+            # If bishop to the top left of king
+            if pos[1] < self.king_x:
+                while True:
+                    pos[0] += 1
+                    pos[1] += 1
 
+                    # You are in check
+                    if self.board[pos[0]][pos[1]] == "K":
+                        self.check = True
+                        return
 
+                    # Hit another piece
+                    if self.board[pos[0]][pos[1]] != ".":
+                        return
 
-        # first check if king is in sight
-        # check for other pieces blocking
-        # possible elimiate some directions by testing where king is
+                    # Hit edge
+                    if pos[0] == 7:
+                        return
+                    if pos[1] == 7:
+                        return
 
+            # Else bishop on the top right of king
+            else:
+                while True:
+                    pos[0] += 1
+                    pos[1] -= 1
 
-    def rook(self, pos):
-        pass
+                    # You are in check
+                    if self.board[pos[0]][pos[1]] == "K":
+                        self.check = True
+                        return
 
-    def bishops(self,pos):
-        pass
+                    # Hit another piece
+                    if self.board[pos[0]][pos[1]] != ".":
+                        return
+
+                    # Hit edge
+                    if pos[0] == 8:
+                        return
+                    if pos[1] == 0:
+                        return
+
+        # Else bishop below king
+        else:
+            # If bishop to the bottom left of king
+            if pos[1] < self.king_x:
+                while True:
+                    pos[0] -= 1
+                    pos[1] += 1
+
+                    # You are in check
+                    if self.board[pos[0]][pos[1]] == "K":
+                        self.check = True
+                        return
+
+                    # Hit another piece
+                    if self.board[pos[0]][pos[1]] != ".":
+                        return
+
+                    # Hit edge
+                    if pos[0] == 0:
+                        return
+                    if pos[1] == 7:
+                        return
+
+            # Else bishop on the bottom right of king
+            else:
+                while True:
+                    pos[0] -= 1
+                    pos[1] -= 1
+
+                    # You are in check
+                    if self.board[pos[0]][pos[1]] == "K":
+                        self.check = True
+                        return
+
+                    # Hit another piece
+                    if self.board[pos[0]][pos[1]] != ".":
+                        return
+
+                    # Hit edge
+                    if pos[0] == 0:
+                        return
+                    if pos[1] == 0:
+                        return
 
     def knight(self, pos):
-        pass
+        """
+        knight: makes sure the bounds are valid then checks every spot for king
+        :param pos: list of two ints containing the position of the knight
+        :return: N/A
+        """
+        if pos[1] <= 6:
+            if self.board[pos[0] - 2][pos[1] + 1] == "K":
+                self.check = True
+        if pos[1] <= 5:
+            if self.board[pos[0] - 1][pos[1] + 2] == "K":
+                self.check = True
+        if pos[0] <= 5 and pos[1] <= 6:
+            if self.board[pos[0] + 2][pos[1] + 1] == "K":
+                self.check = True
+        if pos[0] <= 6 and pos[1] <= 5:
+            if self.board[pos[0] + 1][pos[1] + 2] == "K":
+                self.check = True
+        if pos[0] <= 6:
+            if self.board[pos[0] + 1][pos[1] - 2] == "K":
+                self.check = True
+        if pos[0] <= 5:
+            if self.board[pos[0] + 2][pos[1] - 1] == "K":
+                self.check = True
+        if self.board[pos[0] - 1][pos[1] - 2] == "K":
+            self.check = True
+        if self.board[pos[0] - 2][pos[1] - 1] == "K":
+            self.check = True
 
     def pawn(self, pos):
-        pass
+        """
+        pawn: checks one space diagonally upward for king
+        :param pos: list of two ints containing the position of the pawn
+        :return: N/A
+        """
+        # If pawn is in the top row
+        if pos[0] == 0:
+            return
+        else:
+            if self.king_y == pos[0]-1:
+                if self.king_x == pos[1] + 1 or self.king_x == pos[1] - 1:
+                    self.check = True
+                    return
+            return
+
 
 
 Chess([
@@ -134,5 +265,5 @@ Chess([
     ['.', '.', '.', '.', '.', '.', '.', 'R'],
     ['.', '.', 'N', '.', '.', '.', '.', '.'],
     ['.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', 'Q', '.', '.'],
+    ['.', '', '.', '.', '.', 'Q', '.', '.'],
 ])
