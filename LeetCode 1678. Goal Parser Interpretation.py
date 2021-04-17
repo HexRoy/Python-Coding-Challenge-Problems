9e8a547e028b0c99b5777473a2802b49f9098f95
Geoffroy Penny <GeoffroyPenny@hotmail.com>
2021-04-17 09:34:52 -0400

    done

---
+++ LeetCode 1678. Goal Parser Interpretation.py
@@ -0,0 +1,61 @@
+# Problem
+# =====================================================================================================================
+# You own a Goal Parser that can interpret a string command. The command consists of an alphabet of
+# "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()"
+# as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+#
+# Given the string command, return the Goal Parser's interpretation of command.
+#
+#
+#
+# Example 1:
+#
+# Input: command = "G()(al)"
+# Output: "Goal"
+# Explanation: The Goal Parser interprets the command as follows:
+# G -> G
+# () -> o
+# (al) -> al
+# The final concatenated result is "Goal".
+# Example 2:
+#
+# Input: command = "G()()()()(al)"
+# Output: "Gooooal"
+# Example 3:
+#
+# Input: command = "(al)G(al)()()G"
+# Output: "alGalooG"
+#
+#
+# Constraints:
+#
+# 1 <= command.length <= 100
+# command consists of "G", "()", and/or "(al)" in some order.
+
+
+# Solution
+# ====================================================================================================================
+# Solution ~ 20 ms
+class Solution:
+    def interpret(self, command: str) -> str:
+        result = ''
+        i = 0
+        while i < len(command):
+            if command[i] == 'G':
+                i += 1
+                result += 'G'
+            elif command[i:i + 2] == '()':
+                i += 2
+                result += 'o'
+            elif command[i:i + 4] == '(al)':
+                i += 4
+                result += 'al'
+
+        return result
+
+
+# Input
+# =====================================================================================================================
+obj = Solution()
+print(obj.interpret("G()(al)"))
+
