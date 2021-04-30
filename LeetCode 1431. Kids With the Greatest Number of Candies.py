2c97f98cab87e4cc426694ce9bff24fcace956ba
Geoffroy Penny <geoffroypenny@hotmail.com>
2021-04-30 12:22:33 -0400

    done

--- LeetCode 1431. Kids With the Greatest Number of Candies.py
+++ LeetCode 1431. Kids With the Greatest Number of Candies.py
@@ -4,7 +4,7 @@
 # where candies[i] represents the number of candies that the ith kid has.
 #
 # For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have
-# the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+# the greatest number of candies among them. Notice that multiple kids can have the greategst number of candies.
 #
 #
 #
