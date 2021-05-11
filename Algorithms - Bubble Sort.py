aa83d5e4b7102d7f73851c26e3fcded3ca8f25e5
Geoffroy Penny <GeoffroyPenny@hotmail.com>
2021-05-11 18:39:44 -0400

    done

---
+++ Algorithms - Bubble Sort.py
@@ -0,0 +1,84 @@
+def bubble_sort(elements):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            if elements[j] > elements[j+1]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
+
+
+def bubble_sort_by_key(elements, key):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            if elements[j][key] > elements[j+1][key]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
+
+
+if __name__ == '__main__':
+    elements = [5,9,2,1,67,34,88,34]
+    elements = [1,2,3,4,2]
+    #elements = ["mona", "dhaval", "aamir", "tina", "chang"]
+
+    bubble_sort(elements)
+    print(elements)
+
+# Bubble Sort Exercise
+    # Modify bubble_sort function such that it can sort following list of transactions happening in an electronic store,
+    #
+    # elements = [
+    #         { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+    #         { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+    #         { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+    #         { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    #     ]
+    # bubble_sort function should take key from a transaction record and sort the list as per that key. For example,
+    #
+    # bubble_sort(elements, key='transaction_amount')
+    # This will sort elements by transaction_amount and your sorted list will look like,
+    #
+    # elements = [
+    #         { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+    #         { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+    #         { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    #         { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+    #     ]
+    # But if you call it like this,
+    #
+    # bubble_sort(elements, key='name')
+    # output will be,
+    #
+    # elements = [
+    #         { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    #         { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+    #         { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+    #         { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+    #     ]
+
+    elements = [
+        {'name': 'mona', 'transaction_amount': 1000, 'device': 'iphone-10'},
+        {'name': 'dhaval', 'transaction_amount': 400, 'device': 'google pixel'},
+        {'name': 'kathy', 'transaction_amount': 200, 'device': 'vivo'},
+        {'name': 'aamir', 'transaction_amount': 800, 'device': 'iphone-8'},
+    ]
+    bubble_sort_by_key(elements, 'name')
+    print(elements)
+    bubble_sort_by_key(elements, 'transaction_amount')
+    print(elements)
+    bubble_sort_by_key(elements, 'device')
+    print(elements)
